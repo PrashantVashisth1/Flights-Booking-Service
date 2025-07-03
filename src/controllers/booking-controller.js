a66bca7dff70adeaabfa202e049de7e85b52d554
PrashantVashisth <prashantvashisth2004@gmail.com>
2025-07-03 12:40:36 +0530

    Added the idempotency key

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,40 +1,57 @@
 const { StatusCodes } = require("http-status-codes");
 
-const  { BookingService } = require('../services');
+const { BookingService } = require("../services");
 const { SuccessResponse, ErrorResponse } = require("../utils/common");
 
+const inMemDb = {};
 
 async function createBooking(req, res) {
-  try { 
+  try {
     const flight = await BookingService.createBooking({
       flightId: req.body.flightId,
       userId: req.body.userId,
-      noofSeats: req.body.noofSeats
+      noofSeats: req.body.noofSeats,
     });
     SuccessResponse.data = flight;
     return res.status(StatusCodes.CREATED).json(SuccessResponse);
   } catch (error) {
     ErrorResponse.error = error;
-    return res.status(error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
+    return res
+      .status(error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(ErrorResponse);
   }
 }
 
 async function makePayment(req, res) {
-  try { 
+  try {
+    const idempotencyKey = req.headers["x-idempotency-key"];
+    if (!idempotencyKey) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "idempotency key missing" });
+    }
+    if (inMemDb[idempotencyKey]) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "Cannot retry on a successful payment" });
+    }
     const flight = await BookingService.makePayment({
       bookingId: req.body.bookingId,
       totalCost: req.body.totalCost,
-      userId: req.body.userId
+      userId: req.body.userId,
     });
+    inMemDb[idempotencyKey] = idempotencyKey;
     SuccessResponse.data = flight;
     return res.status(StatusCodes.CREATED).json(SuccessResponse);
   } catch (error) {
     ErrorResponse.error = error;
-    return res.status(error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
+    return res
+      .status(error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(ErrorResponse);
   }
 }
 
 module.exports = {
   createBooking,
-  makePayment
-}
+  makePayment,
+};
